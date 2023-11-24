c8009589cfbcef1e1f1bd5383ef593ac878253b0
unknown <sayemsami7@gmail.com>
2023-11-24 11:06:15 +0600

    Add dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,154 @@
-import Image from 'next/image'
+import {
+  RectangleGroupIcon,
+  TableCellsIcon,
+  BanknotesIcon,
+  BellIcon,
+  UserIcon,
+  ArrowLeftOnRectangleIcon,
+  ClipboardDocumentListIcon,
+  CircleStackIcon,
+} from "@heroicons/react/24/solid";
+import { ClockIcon } from "@heroicons/react/24/outline";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="p-4 bg-slate-200 min-h-screen">
+      <div className="flex gap-4">
+        <div className="w-72 flex flex-col p-3 bg-slate-900 rounded-lg gap-3">
+          <header className="w-full text-white text-sm text-center p-5 border-b-2 border-b-gray-600">
+            Admin DashBoard
+          </header>
+          <div className="flex flex-col gap-1">
+            <div className="flex gap-3 w-full text-white text-sm p-4 rounded-lg hover:bg-zinc-600">
+              <RectangleGroupIcon className="w-5" />
+              <label>Dashboard</label>
+            </div>
+            <div className="flex gap-3 w-full text-white text-sm p-4 rounded-lg hover:bg-zinc-600">
+              <TableCellsIcon className="w-5" />
+              <label>Tables</label>
+            </div>
+            <div className="flex gap-3 w-full text-white text-sm p-4 rounded-lg hover:bg-zinc-600">
+              <BanknotesIcon className="w-5" />
+              <label>Billing</label>
+            </div>
+            <div className="flex gap-3 w-full text-white text-sm p-4 rounded-lg hover:bg-zinc-600">
+              <BellIcon className="w-5" />
+              <label>Notifications</label>
+            </div>
+          </div>
+          <div className="flex flex-col gap-1">
+            <header className="w-full text-white text-sm pl-4 p-2">
+              Account Pages
+            </header>
+            <div className="flex gap-3 w-full text-white text-sm p-4 rounded-lg hover:bg-zinc-600">
+              <UserIcon className="w-5" />
+              <label>Profile</label>
+            </div>
+            <div className="flex gap-3 w-full text-white text-sm p-4 rounded-lg hover:bg-zinc-600">
+              <ArrowLeftOnRectangleIcon className="w-5" />
+              <label>Sign In</label>
+            </div>
+            <div className="flex gap-3 w-full text-white text-sm p-4 rounded-lg hover:bg-zinc-600">
+              <ClipboardDocumentListIcon className="w-5" />
+              <label>Sign Up</label>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col gap-4 w-full">
+          <header className="text-base font-semibold">Dashboard</header>
+          <div className="grid grid-cols-4 gap-6 mt-4">
+            <div className="flex flex-col p-4 gap-4 bg-white rounded-lg">
+              <div className="flex justify-between items-center pb-4 border-b-2 border-b-gray-300">
+                <CircleStackIcon className="w-14 p-4 mt-[-60px] absolute bg-black text-white rounded-lg" />
+                <div className="flex flex-col items-end ml-auto">
+                  <header className="text-sm">Today{"'"}s Money</header>
+                  <h1 className="text-2xl font-extrabold">{"$53k"}</h1>
+                </div>
+              </div>
+              <div className="">+55% than last week</div>
+            </div>
+            <div className="flex flex-col p-4 gap-4 bg-white rounded-lg">
+              <div className="flex justify-between items-center pb-4 border-b-2 border-b-gray-300">
+                <UserIcon className="w-14 p-4 mt-[-60px] absolute bg-pink-600 text-white rounded-lg" />
+                <div className="flex flex-col items-end ml-auto">
+                  <header className="text-sm">Today{"'"}s Users</header>
+                  <h1 className="text-2xl font-extrabold">{"2300"}</h1>
+                </div>
+              </div>
+              <div className="">+55% than last week</div>
+            </div>
+            <div className="flex flex-col p-4 gap-4 bg-white rounded-lg">
+              <div className="flex justify-between items-center pb-4 border-b-2 border-b-gray-300">
+                <UserIcon className="w-14 p-4 mt-[-60px] absolute bg-lime-600 text-white rounded-lg" />
+                <div className="flex flex-col items-end ml-auto">
+                  <header className="text-sm">New Clients</header>
+                  <h1 className="text-2xl font-extrabold">{"3462"}</h1>
+                </div>
+              </div>
+              <div className="">+55% than last week</div>
+            </div>
+            <div className="flex flex-col p-4 gap-4 bg-white rounded-lg">
+              <div className="flex justify-between items-center pb-4 border-b-2 border-b-gray-300">
+                <CircleStackIcon className="w-14 p-4 mt-[-60px] absolute bg-blue-500 text-white rounded-lg " />
+                <div className="flex flex-col items-end ml-auto">
+                  <header className="text-sm">Today{"'"}s Money</header>
+                  <h1 className="text-2xl font-extrabold">{"$103430"}</h1>
+                </div>
+              </div>
+              <div className="">+55% than last week</div>
+            </div>
+          </div>
+          <div className="grid grid-cols-3 gap-6">
+            <div className="flex flex-col p-4 bg-white rounded-lg">
+              <div className="text-center p-20">the chart</div>
+              <div className="flex flex-col mt-auto">
+                <div className="flex flex-col pb-4 border-b-2 border-b-gray-300">
+                  <header className="font-bold">Website Views</header>
+                  <h1 className="text-sm font-extralight">
+                    Last Campaign Performance
+                  </h1>
+                </div>
+                <div className="flex gap-1 mt-4">
+                  <ClockIcon className="w-4" />
+                  <h1 className="text-xs font-extralight">
+                    Campaign sent 2 days ago
+                  </h1>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col p-4 bg-white rounded-lg">
+              <div className="text-center p-20">the chart</div>
+              <div className="flex flex-col mt-auto">
+                <div className="flex flex-col pb-4 border-b-2 border-b-gray-300">
+                  <header className="font-bold">Daily Sales</header>
+                  <h1 className="text-sm font-extralight">
+                    {"+15%"} increase in today sales
+                  </h1>
+                </div>
+                <div className="flex gap-1 mt-4">
+                  <ClockIcon className="w-4" />
+                  <h1 className="text-xs font-extralight">updated 4 min ago</h1>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col p-4 bg-white rounded-lg">
+              <div className="text-center p-20">the chart</div>
+              <div className="flex flex-col mt-auto">
+                <div className="flex flex-col pb-4 border-b-2 border-b-gray-300">
+                  <header className="font-bold">Completed Tasks</header>
+                  <h1 className="text-sm font-extralight">
+                    Last Campaign Performance
+                  </h1>
+                </div>
+                <div className="flex gap-1 mt-4">
+                  <ClockIcon className="w-4" />
+                  <h1 className="text-xs font-extralight">just updated</h1>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
